8562abceef4f868a5c9704bb4d0b804935b417ee
Benjamin Cherry <ben@devcarbon.com>
2023-03-14 22:01:52 -0800

    autosave jorana.el after save

--- jorana.el
+++ jorana.el
@@ -140,16 +140,15 @@ LET-BINDINGS and BODY are the same as in #'let*."
                              (car binding)))
                      let-bindings))))
 
-(defun transclusion-info ()
+(defun transclusion-info () ;<id:1678859998>
   (interactive)
   (alist-of-let*
    ((transcluder (get-text-property (point) 'org-transclusion-by))
     (tc-pair (get-text-property (point) 'org-transclusion-pair))
-    (transcludee (save-buffer
-                  (save-mark-and-excursion
-                    (switch-to-buffer (overlay-buffer tc-pair))
-                    (goto-char (overlay-start tc-pair))
-                    (point-marker))))
+    (transcludee (save-mark-and-excursion
+                   (with-current-buffer (overlay-buffer tc-pair)
+                     (goto-char (overlay-start tc-pair))
+                     (point-marker))))
     ;; When org-transclusion-by is present, we are at source.
     ;; Otherwise we are A. not in a transclusion at all,
     ;; or B. at the transcluder.
@@ -161,6 +160,29 @@ LET-BINDINGS and BODY are the same as in #'let*."
                          (org-babel-mark-block)
                          (region-beginning))
                      transcludee)))))
+
+
+(defun transclusion-infon (buffer marker)
+  (with-current-buffer buffer
+    (alist-of-let*
+     ((transcluder (get-text-property marker 'org-transclusion-by))
+      (tc-pair (get-text-property marker 'org-transclusion-pair))
+      (transcludee (save-buffer
+                    (save-mark-and-excursion
+                      (with-current-buffer (overlay-buffer tc-pair)
+                        (goto-char (overlay-start tc-pair))
+                        (point-marker)))))
+      ;; When org-transclusion-by is present, we are at source.
+      ;; Otherwise we are A. not in a transclusion at all,
+      ;; or B. at the transcluder.
+      (at-transcluder (and (not transcluder) transcludee))
+      (mirror-start (or transcluder transcludee))
+      (in-src-block (equal "src" (get-text-property mirror-start `org-transclusion-type)))
+      (current-start (if (and at-transcluder in-src-block)
+                         (save-mark-and-excursion
+                           (org-babel-mark-block)
+                           (region-beginning))
+                       transcludee))))))
 
 (defun transclusion-mirror-start () ;<id:1678842902>
   "Matching transclusion start marker."
