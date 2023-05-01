14a29dfacd7a17f93cd31f35b761db15abe831db
Benjamin Cherry <Ben@devcarbon.com>
2023-05-01 02:06:23 -0800

    Add nameless name to dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((nameless-current-name . "jorana")))
+ (org-mode . ((nameless-current-name . "jorana"))))
