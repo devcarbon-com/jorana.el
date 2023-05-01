bf7ca60778d143b99f509be76a00a836d6001702
Benjamin Cherry <ben@devcarbon.com>
2023-04-30 18:47:30 -0800

    Remove names in favor of nameless

--- jorana.el
+++ jorana.el
@@ -1,4 +1,4 @@
-;;; package --- Summary
+;;; jorana --- Summary
 ;;; Package-Requires: ((names "") (transient "") (consult "") (projectile "") (emacs "24"))
 
 
@@ -7,16 +7,14 @@
 ;;; Code:
 
 (provide 'jorana)
-(eval-when-compile (require 'names))
 
 
 (use-package transient)
-(define-namespace jorana-
 
-(defcustom current-narrative nil ;<id:1678875463>
+(defcustom jorana-current-narrative nil ;<id:1678875463>
   "Define the narrative for the current project.")
 
-(defvar id-regex (concat "\\(#_\\|;\\|//\\|\\)\\( ?\\)\(?<id:.*?>\)?")
+(defvar jorana-id-regex (concat "\\(#_\\|;\\|//\\|\\)\\( ?\\)\(?<id:.*?>\)?")
   "Regex that matches target tag comments.
 
    Example matches:
@@ -26,10 +24,9 @@
    //<id:tag>
    <id:tag>")
 
-(defvar thing-to-use "sexp")
+(defvar jorana-thing-to-use "sexp")
 
-
-(defmacro alist-of-let* (let-bindings &rest body)
+(defmacro jorana-alist-of-let* (let-bindings &rest body)
   "This is the same as #'let*, except it return an alist of the bindings.
 LET-BINDINGS and BODY are the same as in #'let*."
   `(let* ,let-bindings
@@ -40,83 +37,83 @@ LET-BINDINGS and BODY are the same as in #'let*."
                              (car binding)))
                      let-bindings))))
 
-(defun get-or-create-buffer-for-file (filepath)
+(defun jorana-get-or-create-buffer-for-file (filepath)
   "Return the buffer for FILEPATH, or open buffer if it does not yet exist.
 We don't just use 'find-file-noselect because it would not include unsaved changes."
   (or (find-buffer-visiting filepath)
       (find-file-noselect filepath)))
 
-(defun current-time-in-seconds ()
+(defun jorana-current-time-in-seconds ()
   "Return the current time in seconds from the epoch."
   (format-time-string "%s" (current-time)))
 
-(defun get-line-contents (line)
+(defun jorana-get-line-contents (line)
   "Return the line contents for LINE, with leading and trailing whitespace trimmed."
   (let ((start (line-beginning-position line))
         (end (line-end-position line)))
     (string-trim (buffer-substring start end))))
 
-(defun remove-non-symbol-chars (string)
+(defun jorana-remove-non-symbol-chars (string)
   "Replace all non link-safe characters in STRING with underscores."
   (replace-regexp-in-string "[^a-zA-Z0-9_]" "_" string))
 
 
 ;;; * ids
 
-(defun gen-id-tag ()
-  (format "<id:%s>" (current-time-in-seconds)))
+(defun jorana-gen-id-tag ()
+  (format "<id:%s>" (jorana-current-time-in-seconds)))
 
-(defun insert-id-tag () ;<id:1672227875>
+(defun jorana-insert-id-tag () ;<id:1672227875>
   "Insert a jorana id. Currently, the current time in seconds from the epoch."
   (interactive)
-  (insert (gen-id-tag)))
+  (insert (jorana-gen-id-tag)))
 
-(defun hide-id-regex () ;<id:1672243157>
+(defun jorana-hide-id-regex () ;<id:1672243157>
   "Hide."
   (interactive)
   (save-excursion
     (goto-char (point-min))
-    (while (re-search-forward id-regex nil t)
+    (while (re-search-forward jorana-id-regex nil t)
       (add-text-properties (match-beginning 0) (match-end 0) '(invisible t)))
     (set-buffer-modified-p nil)))
 
-(defun toggle-id-visibility () ;
+(defun jorana-toggle-id-visibility () ;
   "Toggle the visibility id markers IE. `#_(id:<number>)`."
   (interactive)
   (save-excursion
     (goto-char (point-min))
-    (while (re-search-forward id-regex nil t)
+    (while (re-search-forward jorana-id-regex nil t)
       (let ((invisible (get-text-property (match-beginning 0) 'invisible)))
         (if invisible
             (remove-text-properties (match-beginning 0) (match-end 0) '(invisible t))
           (add-text-properties (match-beginning 0) (match-end 0) '(invisible t))))
       (set-buffer-modified-p nil))))
 
-(defun hide-id-regex () ;<id:1672242977>
+(defun jorana-hide-id-regex () ;<id:1672242977>
   "Hide."
   (interactive)
   (save-excursion
     (goto-char (point-min))
-    (while (re-search-forward id-regex nil t)
+    (while (re-search-forward jorana-id-regex nil t)
       (add-text-properties (match-beginning 0) (match-end 0) '(invisible t)))
     (set-buffer-modified-p nil)))
 
 ;;; * misc
 
-(defun current-non-hidden-buffers ()
+(defun jorana-current-non-hidden-buffers ()
   "Return a list of the current non-hidden buffers."
   (interactive)
   (let ((buffers (buffer-list)))
     (seq-filter (lambda (buffer) (not (string-prefix-p " " (buffer-name buffer)))) buffers)))
-(defun current-project-buffers ()
+(defun jorana-current-project-buffers ()
   "Return a list of current-non-hidden buffers that are of the current project."
   (let ((project-dir (projectile-project-root))) 
     (cl-remove-if-not (lambda (buf)
                         (and (buffer-file-name buf)
                              (string-prefix-p project-dir (file-name-directory (buffer-file-name buf))))) 
-                      (current-non-hidden-buffers))))
+                      (jorana-current-non-hidden-buffers))))
 
-(defun create-org-link-to-line ()
+(defun jorana-create-org-link-to-line ()
   "Create an Org mode link to the contents of the line at point."
   (interactive)
   (let* ((file-name (buffer-file-name))
@@ -127,31 +124,31 @@ We don't just use 'find-file-noselect because it would not include unsaved chang
 
 ;;; * marker utils
 
-(defun marker-at-line (line) ;<id:1672242453>
+(defun jorana-marker-at-line (line) ;<id:1672242453>
   "Create a marker at the beginning of LINE in the current buffer."
   (set-marker (make-marker) (line-beginning-position line)))
 
-(defun marker-at-point (point &rest buffer) ;<id:1672242453>
+(defun jorana-marker-at-point (point &rest buffer) ;<id:1672242453>
   "Create a marker at POINT in BUFFER."
   (set-marker (make-marker) point (car (or buffer (current-buffer)))))
 
-(defun append-to-line (line string)
+(defun jorana-append-to-line (line string)
   "Append STRING to the end of LINE in the current buffer."
   (end-of-line)
   (insert string))
 
-(defun -goto-marker (marker)
+(defun jorana--goto-marker (marker)
   (switch-to-buffer (marker-buffer marker))
   (goto-char marker))
 ;;; -- 
 ;;; * Target
 
-(defun target-at-point ()
+(defun jorana-target-at-point ()
   (interactive)
-  (extract-target-from-line (line-number-at-pos (point)) t)
+  (jorana-extract-target-from-line (line-number-at-pos (point)) t)
   )
 
-(defun target-from-line (line) ;<id:1678573021>
+(defun jorana-target-from-line (line) ;<id:1678573021>
   "Target from LINE."
   (let ((on-org-heading (and (eq major-mode 'org-mode)
                              (org-on-heading-p))))
@@ -164,30 +161,30 @@ We don't just use 'find-file-noselect because it would not include unsaved chang
       (prog2 (string-match "\\(<.*>\\)" line-text)
           (match-string 1 line-text)))))
 
-(defun extract-target-from-line! (line &optional generate-when-missing comment-string) ;<id:1678630244>
+(defun jorana-extract-target-from-line! (line &optional generate-when-missing comment-string) ;<id:1678630244>
   "Extract the target from LINE using a regex that matches a jorana-id.
 GENERATE-WHEN-MISSING adds an id if one doesn't already exist. 
 COMMENT-STRING is the comment to use to prefix the id.
 where [anything] is one or more characters. Return an Org mode link to the target."
   (let* ((line-text (buffer-substring (line-beginning-position) (line-end-position)))
-         (target (target-from-line line)))
+         (target (jorana-target-from-line line)))
     (cond (target target)
           (generate-when-missing 
-           (let ((target (gen-id-tag)))
+           (let ((target (jorana-gen-id-tag)))
              (unless (eq major-mode 'org-mode)
-               (append-to-line line (concat (or comment-string " ;") target)))
+               (jorana-append-to-line line (concat (or comment-string " ;") target)))
              target))
           (:no-target-found line))))
 
-(defun create-link-target! () ;<id:1678574962>
+(defun jorana-create-link-target! () ;<id:1678574962>
   (let* ((root (projectile-project-root))
          (file buffer-file-name)
          (relative-file (concat "file:" (file-relative-name file root)))
          (link (let* ((line (line-number-at-pos (point-marker)))
                       (line-point (bounds-of-thing-at-point 'line)))
                  (list :line line 
-                       :target (extract-target-from-line! line t "  ;")
-                       :text (remove-non-symbol-chars (buffer-substring (car line-point) (- (cdr line-point) 1))))))
+                       :target (jorana-extract-target-from-line! line t "  ;")
+                       :text (jorana-remove-non-symbol-chars (buffer-substring (car line-point) (- (cdr line-point) 1))))))
          (line (plist-get link :line))
          (line-string (plist-get link :text))
          (target (plist-get link :target))
@@ -199,7 +196,7 @@ where [anything] is one or more characters. Return an Org mode link to the targe
 
 ;;; * Jumping
 
-(defun marker-of-mirrored-point (mirror-start cursor-offset)
+(defun jorana-marker-of-mirrored-point (mirror-start cursor-offset)
   "Create marker to matching transclusion."
   (save-excursion
     (with-current-buffer (marker-buffer mirror-start)
@@ -207,8 +204,8 @@ where [anything] is one or more characters. Return an Org mode link to the targe
       (goto-char (+ (point) cursor-offset))
       (point-marker))))
 
-(defun transclusion-info () ;<id:1678859998>
-  (alist-of-let*
+(defun jorana-transclusion-info () ;<id:1678859998>
+  (jorana-alist-of-let*
    ((transcluder (get-char-property (point) 'org-transclusion-by))
     (tc-pair (get-char-property (point) 'org-transclusion-pair))
     (transcludee (save-mark-and-excursion
@@ -237,11 +234,11 @@ where [anything] is one or more characters. Return an Org mode link to the targe
                           (set-marker (make-marker) (region-beginning))))
                     mirror-start)))))
 
-(defun -mirror-offset (current-start)
+(defun jorana--mirror-offset (current-start)
   (with-current-buffer (marker-buffer current-start)
     (- (point) current-start)))
 
-(defun refresh-remote-transclusion ()
+(defun jorana-refresh-remote-transclusion ()
   (let ((remote (get-char-property (point) 'org-transclusion-by)))
     (when remote
       (with-current-buffer (marker-buffer remote)
@@ -249,22 +246,22 @@ where [anything] is one or more characters. Return an Org mode link to the targe
           (goto-char remote)
           (org-transclusion-refresh))))))
 
-(defun jump-to-transclusion-pair () ;<id:1678874579>
+(defun jorana-jump-to-transclusion-pair () ;<id:1678874579>
   "Goto matching transclusion."
   (interactive)
-  (refresh-remote-transclusion)
+  (jorana-refresh-remote-transclusion)
   ;; Let's try to not jar the view too much.
   (let ((current-scroll-pos (count-lines (window-start) (point))))
-    (let-alist (transclusion-info)
+    (let-alist (jorana-transclusion-info)
       ;; try to re-trigger fontlock coloring by scrolling up first.
-      (-goto-marker .mirror-start)
+      (jorana--goto-marker .mirror-start)
       (recenter -1)
-      (-goto-marker (marker-of-mirrored-point .mirror-start (-mirror-offset .current-start)))
+      (jorana--goto-marker (jorana-marker-of-mirrored-point .mirror-start (jorana--mirror-offset .current-start)))
       (recenter current-scroll-pos))))
 
 
 ;;; * Linking
-(defun find-file-line-link! () ;<id:1672243830>
+(defun jorana-find-file-line-link! () ;<id:1672243830>
   "Jump to the most recent project buffer;
 Then prompt user to navigate to the code they want to include.
 
@@ -275,56 +272,56 @@ The returned plist contains the following keys:
 :target - the target of the link. This is an id like '<id:1172243297>'
           so that links still work after refactoring."
   (let ((return-buffer (current-buffer)))
-    (switch-to-buffer (cadr (current-project-buffers)))
+    (switch-to-buffer (cadr (jorana-current-project-buffers)))
     (message (substitute-command-keys "Jump to the code you want to include, and press \\[exit-recursive-edit] to finish."))
     (recursive-edit)
     (let* ((root (projectile-project-root))
            (relative-file (concat "file:" (file-relative-name buffer-file-name root)))
            (code-buffer (current-buffer)))
       (switch-to-buffer return-buffer)
-      (with-current-buffer code-buffer (create-link-target!)))))
+      (with-current-buffer code-buffer (jorana-create-link-target!)))))
 
-(defun transclusion-link-from-target (target)
+(defun jorana-transclusion-link-from-target (target)
   (let* ((line-link target)
          (link (plist-get line-link :link))
          (file (plist-get line-link :file))
          
-         (target-info (let ((buffer (get-or-create-buffer-for-file file)))
+         (target-info (let ((buffer (jorana-get-or-create-buffer-for-file file)))
                         (with-current-buffer buffer
                           (list :lang (downcase (if (listp mode-name) (car mode-name) mode-name)))))))
     (let ((lang (plist-get target-info :lang)))
       (cond ((string= lang "org") (format "#+transclude: %s" link))
-            (t (format "#+transclude: %s :src %s :thing-at-point %s" link lang thing-to-use))))))
+            (t (format "#+transclude: %s :src %s :thing-at-point %s" link lang jorana-thing-to-use))))))
 
-(defun copy-transclusion-link ()
+(defun jorana-copy-transclusion-link ()
   (interactive)
-  (kill-new (transclusion-link-from-target (create-link-target!))))
+  (kill-new (jorana-transclusion-link-from-target (jorana-create-link-target!))))
 
-(defun add-to-narrative () ;<id:1678580234>
+(defun jorana-add-to-narrative () ;<id:1678580234>
   (interactive)
-  (let ((narrative (or current-narrative (jorana-set-narrative (read-file-name "Narrative: " jorana-current-narrative))))
+  (let ((narrative (or jorana-current-narrative (jorana-set-narrative (read-file-name "Narrative: " jorana-current-narrative))))
         (code-buf (current-buffer)))
-    (switch-to-buffer (get-or-create-buffer-for-file narrative))
+    (switch-to-buffer (jorana-get-or-create-buffer-for-file narrative))
     (message (substitute-command-keys "Go to where you want the transclution, and press \\[exit-recursive-edit]"))
     (recursive-edit)
-    (insert (transclusion-link-from-target (with-current-buffer code-buf (create-link-target!))))
+    (insert (jorana-transclusion-link-from-target (with-current-buffer code-buf (jorana-create-link-target!))))
     (org-transclusion-add)))
 
-(defun find-and-insert-transclusion ()  ;<id:1678618587>
+(defun jorana-find-and-insert-transclusion ()  ;<id:1678618587>
   "Find file and thing to transclude into current buffer."
   (interactive)
-  (insert (transclusion-link-from-target (find-file-line-link!)))
-  (org-transclusion-add)))
+  (insert (jorana-transclusion-link-from-target (jorana-find-file-line-link!)))
+  (org-transclusion-add))
 
-(defun insert-file-link () ;<id:1672242704>
+(defun jorana-insert-file-link () ;<id:1672242704>
   (interactive)
-  (insert (plist-get (find-file-line-link) :link)))
+  (insert (plist-get (jorana-find-file-line-link) :link)))
 
 
 
 ;;; * User interface
 
-(transient-define-suffix jorana-set-narrative (current-narrative)
+(transient-define-suffix jorana-set-narrative (jorana-current-narrative)
   "Set the sentence from minibuffer read"
   :description '(lambda ()
                   (concat
